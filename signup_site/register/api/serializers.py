ce85178f456de8ac6ade1c5bb74fb1d6835104ea
samghadri <sam.ghadri@gmail.com>
2017-12-17 15:41:40 +0000

    PassWordRegisterAPi

--- signup_site/register/api/serializers.py
+++ signup_site/register/api/serializers.py
@@ -10,5 +10,18 @@ class UserCreateSerializer(ModelSerializer):
             'username',
             'email',
             'password',
-
             ]
+        extra_kwargs ={'password':{'write_only':True}
+        }
+
+    def create(self, validated_data):
+        username = validated_data['username']
+        email = validated_data['email']
+        password = validated_data['password']
+        user_obj = User(
+        username = username,
+        email = email
+        )
+        user_obj.set_password(password)
+        user_obj = save()
+        return validated_data
