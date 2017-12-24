7003913b30afa32b73263c23ea8e8523ad9bbe90
samghadri <sam.ghadri@gmail.com>
2017-12-24 12:45:38 +0000

    creatingPosTModel

--- signup_site/posts/models.py
+++ signup_site/posts/models.py
@@ -1,7 +1,31 @@
 from django.db import models
-from django.django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
 User = get_user_model
+from groups.models import Group
+
 
 
 class Post(models.Model):
     user = models.ForeignKey(User, related_name='posts')
+    title = models.CharField(max_length=50)
+    text = models.CharField()
+    created_date = mdoels.DateTimeField(auto_now=True)
+    group = models.ForeignKey(Group,related_name='groups',null=True,blank=True)
+
+
+    def __str__(self):
+        return self.title
+
+
+    def save(self, *args, **kwarg):
+        super().save(*args, **kwargs)
+
+
+    def get_absolute_url(self):
+        return reverse('posts:single',kwargs={'username':self.username,
+                                              'pk': self.pk})
+
+    class Meta:
+        ordering = ['-created_date']
+        unique_together = ['username', 'title']
