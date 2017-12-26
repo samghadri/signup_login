3e46e209c94bd3110e7ad723e855ada613408d48
samghadri <sam.ghadri@gmail.com>
2017-12-26 14:54:48 +0000

    JoinGroupVIEWSandURLS

--- signup_site/groups/urls.py
+++ signup_site/groups/urls.py
@@ -4,8 +4,10 @@ from . import views
 app_name = 'groups'
 
 urlpatterns = [
-    url(r'new/$',views.CreateGroupView.as_view(), name='create'),
+    url(r'^new/$',views.CreateGroupView.as_view(), name='create'),
     url(r'posts/in/(?P<slug>[-\w]+)/$',views.SingleGroupView.as_view(), name='single'),
     url(r'^$',views.ListGroupView.as_view(), name='all'),
+    url(r'join/(?P<slug>[-\w]+)/$', views.JoinGroupView.as_view(), name='join'),
+    url(r'leave/(?P<slug>[-\w]+)/$', views.LeaveGroupView.as_view(), name='leave'),
 
 ]
--- signup_site/groups/views.py
+++ signup_site/groups/views.py
@@ -31,9 +31,30 @@ class JoinGroupView(LoginRequiredMixin, generic.RedirectView):
             GroupMember.objects.create(user=request.user, group = group)
 
         except IntegrityError:
-            messages.warning(self.request,('You are already member of {}'.format(group.name)))
+            messages.warning(self.request,('Sorry! You are already member of {}'.format(group.name)))
 
         else:
             messages.success(self.request, 'You are now a Member of {}'.format(group.name))
 
         return super().get(request, *args, **kwargs)
+
+class LeaveGroupView(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('groups:all', kwargs={'slug':self.kwargs.get('slug')})
+
+
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group, slug=self.kwargs.get('slug'))
+
+        try:
+            membership = models.GroupMember.objects.filter(
+                user = self.request.user, group__slug=self.kwargs.get('slug'))
+            ).get()
+        except models.GroupMember.DoesNotExist:
+            messages.warning(self.request, 'Sorry You are already Not a member!!')
+
+        else:
+            messages.success(self.request, 'You have successfully left the group')
+        return super().get(request, *args, **kwargs)
+    
